8ff4c89175565b66f2f83addd3ff742a610540bd
Aayush Basnet <aayush9823@gmail.com>
2025-06-15 21:10:30 +0545

    committed

---
+++ Stone, Paper & Scissors Game/app.js
@@ -0,0 +1,48 @@
+let userScore = 0;
+let compScore = 0;
+
+const choices = document.querySelectorAll(".choice");
+
+const genCompChoice = () => {
+  const options = ["rock", "paper", "scissors"];
+  const randIdx = Math.floor(Math.random() * 3);
+  return options[randIdx];
+};
+
+const playGame = (userChoice) => {
+  console.log(userChoice);
+  const compChoice = genCompChoice();
+  console.log(compChoice);
+
+  if (userChoice === compChoice) {
+    drawGame();
+  } else {
+    let userWin = true;
+    if (userChoice === "rock") {
+      userWin = compChoice === "paper" ? false : true;
+    } else if (userChoice === "paper") {
+      userWin = compChoice === "scissors" ? false : true;
+    } else {
+      userWin = compChoice === "rock" ? false : true;
+    }
+    showWinner(userWin);
+  }
+};
+
+choices.forEach((choice) => {
+  choice.addEventListener("click", () => {
+    const userChoice = choice.getAttribute("id");
+    playGame(userChoice);
+  });
+});
+
+const drawGame = () => {
+  console.log("game is draw");
+};
+
+const showWinner = (userWin) => {
+  if (userWin) {
+    console.log("you win");
+  } else {
+  }
+};
